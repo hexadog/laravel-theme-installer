d76c40e766869dd4e79cb6cc2d3eaa54dc1b7fca
Gaetan <gaetan@hexadog.com>
2020-10-25 16:13:53 +0100

    fix(plugin): Add new abstract methods
    Mandatory since Composer 2.0

--- src/ThemeInstallerPlugin.php
+++ src/ThemeInstallerPlugin.php
@@ -17,4 +17,20 @@ class ThemeInstallerPlugin implements PluginInterface
 
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+	/**
+     * {@inheritdoc}
+     */
+	public function deactivate(Composer $composer, IOInterface $io)
+	{
+		// 
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+	public function uninstall(Composer $composer, IOInterface $io)
+	{
+		// 
+	}
 }
